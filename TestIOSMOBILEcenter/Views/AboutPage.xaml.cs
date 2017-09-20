a050c01645786f52ddc7de9621927dd4dfcc9290
efulgencio <eduardofulgenciocomendeiro@gmail.com>
2017-09-20 13:42:43 +0200

    subida 1 vez

---
+++ TestIOSMOBILEcenter/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TestIOSMOBILEcenter
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
